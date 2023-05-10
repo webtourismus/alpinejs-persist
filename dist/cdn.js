97815cc786938455e40810a8a8b7a31fc5ef4cf7
Webtourismus.at <entwicklung@webtourismus.at>
2023-05-10 10:22:00 +0200

    Update to version 3.12.0

--- dist/cdn.js
+++ dist/cdn.js
@@ -24,8 +24,17 @@
         };
       });
     };
-    Object.defineProperty(Alpine, "$persist", {get: () => persist()});
+    Object.defineProperty(Alpine, "$persist", { get: () => persist() });
     Alpine.magic("persist", persist);
+    Alpine.persist = (key, { get, set }, storage = localStorage) => {
+      let initial = storageHas(key, storage) ? storageGet(key, storage) : get();
+      set(initial);
+      Alpine.effect(() => {
+        let value = get();
+        storageSet(key, value, storage);
+        set(value);
+      });
+    };
   }
   function storageHas(key, storage) {
     return storage.getItem(key) !== null;
